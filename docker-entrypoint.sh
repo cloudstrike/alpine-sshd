87eccb2967e62879ca0df6a4c23a812d1dd2b81c
Tomas <tomas+github@libre.nz>
2020-01-13 14:35:19 +1300

    fetch SSH key from URL

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 if [ -z "${AUTHORIZED_KEYS}" ]; then
-  echo "Need your ssh public key as AUTHORIZED_KEYS env variable. Abnormal exit ..."
+  echo "Set your ssh public key as URL inside AUTHORIZED_KEYS env variable. Exiting..."
   exit 1
 fi
 
-echo "Populating /root/.ssh/authorized_keys with the value from AUTHORIZED_KEYS env variable ..."
-echo "${AUTHORIZED_KEYS}" > /root/.ssh/authorized_keys
+echo "Populating /root/.ssh/authorized_keys with the key downloaded from AUTHORIZED_KEYS env variable ..."
+curl -LSo /root/.ssh/authorized_keys "${AUTHORIZED_KEYS}"
 
 # Execute the CMD from the Dockerfile:
 exec "$@"
-
