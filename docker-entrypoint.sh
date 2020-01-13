56981b1dd1175be4811c633e3b910e7ab448fcd8
Tomas <tomas+github@libre.nz>
2020-01-13 15:35:12 +1300

    added SSH_USER to fetch Github automatically

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+SSH_USER=${SSH_USER:-"toronz"}
 if [ -z "${AUTHORIZED_KEYS}" ]; then
-  echo "Set your ssh public key as URL inside AUTHORIZED_KEYS env variable. Exiting..."
+  echo "Missing ssh public key URL inside AUTHORIZED_KEYS env variable. Defaulting to Github SSH public key using SSH_USER..."
+  AUTHORIZED_KEYS="https://github.com/${SSH_USER}.keys"
   exit 1
 fi
 
+adduser --disabled-password --gecos "" "${SSH_USER}"
+
 echo "Populating /root/.ssh/authorized_keys with the key downloaded from AUTHORIZED_KEYS env variable ..."
-curl -LSo /root/.ssh/authorized_keys "${AUTHORIZED_KEYS}"
+curl -LSo /home/"${SSH_USER}"/.ssh/authorized_keys "${AUTHORIZED_KEYS}"
 
 # Execute the CMD from the Dockerfile:
 exec "$@"
